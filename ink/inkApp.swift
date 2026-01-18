c162c64e20fc1b5b17aab7b8862ee199167c9585
Waj <agentwaj@gmail.com>
2026-01-17 21:05:25 -0500

    Initial Commit

---
+++ ink/inkApp.swift
@@ -0,0 +1,17 @@
+//
+//  inkApp.swift
+//  ink
+//
+//  Created by agentwaj on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct inkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
